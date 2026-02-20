2780852fea66aeed6c1386ed17243f10cd225d5d
skiba <skibinskig@gmail.com>
2026-02-20 16:36:48 +0100

    fix: add main.jsx React entry point

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import IceMeltAnalyzer from './IceMeltAnalyzer';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <IceMeltAnalyzer />
+  </React.StrictMode>
+);
